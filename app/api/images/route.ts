7a401856a7e8a0c3e1ba04cdfd8b3919c42dd107
Marcos Laina <marcos.laina@loudintelligence.com>
2024-10-29 20:56:57 +0100

    auth images test

--- app/api/images/route.ts
+++ app/api/images/route.ts
@@ -1,11 +1,18 @@
 import Replicate from "replicate";
 import {NextResponse} from "next/server";
-
+import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
 
 const replicate = new Replicate()
 
-
 export async function POST(req) {
+    const supabase = createRouteHandlerClient({ cookies });
+    const { data: { user } } = await supabase.auth.getUser();
+
+    if (!user) {
+        return NextResponse.json({ error: 'No autorizado' }, { status: 401 });
+    }
+
     const { model, input } = await req.json();
     const output = await replicate.run(model, { input });
     const image = Array.isArray(output) ? output[0] : output;
