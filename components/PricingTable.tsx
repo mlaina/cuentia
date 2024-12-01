d44585991deeaddf248d4135a5df237e9b5cf531
Marcos Laina <marcos.laina@loudintelligence.com>
2024-12-01 21:25:27 +0100

    fix lint

--- components/PricingTable.tsx
+++ components/PricingTable.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { Button } from '@/components/ui/button'
 import { loadStripe } from '@stripe/stripe-js'
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY)
