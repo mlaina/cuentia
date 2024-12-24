7e3b874cc27fa373b842736500493f78b9383eba
Marcos Laina <marcos.laina@loudintelligence.com>
2024-12-24 12:46:22 +0100

    padding y faq

--- components/Accordion.jsx
+++ components/Accordion.jsx
@@ -6,7 +6,7 @@ function Accordion ({ title, children, isOpen, onClick }) {
     <div className='w-full mx-auto'>
       <div className='bg-secondary-100 overflow-hidden rounded-xl'>
         <div
-          className='px-4 py-5 sm:px-6 flex justify-between items-center cursor-pointer'
+          className='px-4 py-4 sm:px-6 flex justify-between items-center cursor-pointer'
           onClick={onClick}
         >
           <h3 className='text-lg leading-6 text-gray-700 font-bold'>{title}</h3>
@@ -21,7 +21,7 @@ function Accordion ({ title, children, isOpen, onClick }) {
             isOpen ? 'max-h-96' : 'max-h-0'
           }`}
         >
-          <div className='bg-ocre-600 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
+          <div className='bg-ocre-600 px-4 pb-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
             <div className='text-gray-600 sm:mt-0 sm:col-span-3'>{children}</div>
           </div>
         </div>
